9dfe94596d55f0d50cc7c3064f8989d8d047416d
NjeriMuchiri <muchirinjeri3210@gmail.com>
2023-11-29 02:08:00 +0300

    using let

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,6 +1,6 @@
 export default function appendToEachArrayValue(array, appendString) {
-  for (let idx of array) {
-    idx = appendString + idx;
+  for (let value of array) {
+    value = appendString + value;
   }
   return array;
 }
