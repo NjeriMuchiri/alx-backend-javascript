23ff0c82760b11c5365f61bfda093fadc8350aaa
NjeriMuchiri <muchirinjeri3210@gmail.com>
2023-11-29 02:45:31 +0300

    loops

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,7 +1,8 @@
 export default function appendToEachArrayValue(array, appendString) {
   for (let value of array) {
-    
+    this.arrayList = [];
     value = appendString + value;
+    this.arrayList.push(value);
   }
   return array;
 }
