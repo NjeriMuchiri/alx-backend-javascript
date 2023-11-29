0ef858aac83885350cf02d1feb4c38013c969555
NjeriMuchiri <muchirinjeri3210@gmail.com>
2023-11-30 01:34:27 +0300

    promise all

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -2,14 +2,14 @@ function handleResponseFromAPI(promise) {
   return promise
     .then(() => {
       console.log('Got a response from the API');
-      return new Promise((resolve, reject) => {
+      return new Promise((resolve) => {
         if (promise) {
           resolve({
             status: 200,
             body: 'success',
           });
         } else {
-          reject();
+          console.error();
         }
       });
     });
