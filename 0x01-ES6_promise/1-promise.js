85da062bdb36da5219352a9edacc22c5bc045514
NjeriMuchiri <muchirinjeri3210@gmail.com>
2023-11-29 23:48:36 +0300

    rejectiong and resolving promises

---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,13 @@
+function getFullResponseFromApi(success) {
+  return new Promise((resolve, reject) => {
+    if (success) {
+      resolve({
+        status: 200,
+        body: 'Success',
+      });
+    } else {
+      reject(new Error('The fake API is not working currently'));
+    }
+  });
+}
+export default getFullResponseFromApi;
