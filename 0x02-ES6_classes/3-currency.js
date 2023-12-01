c4cc57f6a749611406d070cec7f856c1c6219117
NjeriMuchiri <muchirinjeri3210@gmail.com>
2023-12-01 10:14:45 +0300

    class methods

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -1,28 +1,34 @@
 class Currency {
-    constructor(code, name) {
-        this._code = code;
-        this._name = name;
+  constructor(code, name) {
+    this._code = code;
+    this._name = name;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(theCode) {
+    this._code = this.validateString(theCode, 'code');
+  }
+
+  get name() {
+    return this.name;
+  }
+
+  set name(theName) {
+    this.__name = this.validateString(theName, 'name');
+  }
+
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
+
+  validateString(value, attribute) {
+    if (typeof this.value !== 'string') {
+      throw new TypeError(`${attribute} must be a string.`);
     }
-    get code() {
-        return this._code;
-    }
-    set code(theCode) {
-        this._code = this.validateString(theCode, 'code');
-    }
-    get name() {
-        return this.name;
-    }
-    set name(theName) {
-        this.__name = this.validateString(theName, 'name')
-    }
-    displayFullCurrency() {
-        return `${this._name} (${this._code})`;
-    }
-    validateString(value, attribute) {
-        if (typeof this.value !== 'string') {
-          throw new TypeError(`${attribute} must be a string.`);
-        }
-        return value;
-      }
+    return value;
+  }
 }
 export default Currency;
