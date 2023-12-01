f90af9803acb449e36aae341e00ce9b92474aaba
NjeriMuchiri <muchirinjeri3210@gmail.com>
2023-12-01 10:01:55 +0300

    class methods

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,28 @@
+class Currency {
+    constructor(code, name) {
+        this._code = code;
+        this._name = name;
+    }
+    get code() {
+        return this._code;
+    }
+    set code(theCode) {
+        this._code = this.validateString(theCode, 'code');
+    }
+    get name() {
+        return this.name;
+    }
+    set name(theName) {
+        this.__name = this.validateString(theName, 'name')
+    }
+    displayFullCurrency() {
+        return `${this._name} (${this._code})`;
+    }
+    validateString(value, attribute) {
+        if (typeof this.value !== 'string') {
+          throw new TypeError(`${attribute} must be a string.`);
+        }
+        return value;
+      }
+}
+export default Currency;
