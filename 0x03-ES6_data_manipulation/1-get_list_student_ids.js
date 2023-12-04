d8c9d69243d742ab3ecadbaf7717b34bdd31d106
NjeriMuchiri <muchirinjeri3210@gmail.com>
2023-12-04 22:37:09 +0300

    mapping thru the students ids

---
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,7 @@
+function getListStudentsIds(students) {
+  if (!Array.isArray(students)) {
+    return [];
+  }
+  return students.map((student) => student.id);
+}
+export default getListStudentsIds;
