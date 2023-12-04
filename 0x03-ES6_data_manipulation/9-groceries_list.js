bb06157ec86bbc032b92da503d6a40d002af0cb7
NjeriMuchiri <muchirinjeri3210@gmail.com>
2023-12-04 23:17:17 +0300

    new map

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,11 @@
+function groceriesList() {
+  const groceryListMap = new Map([
+    ['Apples', 10],
+    ['Tomatoes', 10],
+    ['Pasta', 1],
+    ['Rice', 1],
+    ['Banana', 5],
+  ]);
+  return groceryListMap;
+}
+export default groceriesList;
